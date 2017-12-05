fcea84c674fefe64f7840d25d148c2895df764eb
Sam <alshuduki@gmail.com>
2017-12-04 21:05:26 -0600

    Update Admin.php

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -172,7 +172,7 @@ class Admin extends CI_Controller {
         $config['smtp_host'] = 'ssl://smtp.gmail.com'; //smtp host name
         $config['smtp_port'] = '465'; //smtp port number
         $config['smtp_user'] = 'bisendatech@gmail.com';
-        $config['smtp_pass'] = 'b123456781'; //$from_email password
+        $config['smtp_pass'] = '****'; //$from_email password
         $config['mailtype'] = 'html';
         $config['charset'] = 'iso-8859-1';
         $config['wordwrap'] = TRUE;
